ad96ebd427957a94eeb0be074490a90a76381a50
Ajit Gupta <ajitg131@gmail.com>
2022-11-11 21:44:22 +0530

    Done Auth

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+require('dotenv').config();
 const port = 3000
 const bodyParser = require("body-parser");
 const path = require('path');
+
+
+const { auth, requiresAuth} = require('express-openid-connect');
+app.use(
+  auth({
+    authRequired : false,
+    auth0Logout: true,
+    issuerBaseURL:process.env.ISSUER_BASE_URL ,
+    baseURL: process.env.BASE_URL,
+    clientID:process.env.CLIENT_ID ,
+    secret: process.env.SECRET ,
+    idpLogout: true,
+  })
+);
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/public', express.static(path.join(__dirname,'./static')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 })
 
-app.get('/home', (req, res) => {
+app.get('/home', requiresAuth(), (req, res) => {
     res.sendFile(path.join(__dirname+'/login.html'));
+
+})
+
+app.get('/callback', (req, res) => {
+  res.send('Hello Callback!')
+
+})
+app.get('/success', (req, res) => {
+  res.send('SUCCESS!')
 
 })
 
